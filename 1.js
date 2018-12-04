07c696a18d0f143c3202c98b1a172cf742f9c312
Evgen097 <k0990245853@gmail.com>
2018-12-04 11:43:55 +0200

    first commit

---
+++ 1.js
@@ -0,0 +1,6 @@
+// echo "# vegetableslider" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/Evgen097/vegetableslider.git
+//     git push -u origin master
